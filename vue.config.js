07f66f5a8fd41ef512d36765201552f17282aa95
WJWYA <392154083@qq.com>
2020-12-10 21:24:38 +0800

    解决跨域问题

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  publicPath: './'
+  publicPath: './',
+  // 解决跨域
+  devServer:{
+    proxy: {
+      '/apis': {
+  
+        // 测试环境
+  
+        target: 'https://autumnfish.cn', // 接口域名
+  
+        changeOrigin: true, //是否跨域
+  
+        pathRewrite: {
+  
+          '^/apis': '' //需要rewrite重写的,
+        }
+      }
+    }
+  }
 }
